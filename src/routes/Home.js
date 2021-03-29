9f8735bf6c849a5e68c3dd8645f73b4eb7b4abf5
ezh29 <ezh29@naver.com>
2021-03-29 18:40:23 +0900

    Realtime Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,31 +1,26 @@
 import { dbService } from "fbase";
 import React, { useState, useEffect } from "react";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-  const getNweets = async () => {
-    const dbNweets = await dbService.collection("nweets").get();
-    dbNweets.forEach((document) => {
-      const nweetObject = {
-        ...document.data(), //es6 spread attribute 전개구문
-        id: document.id,
-      };
-      setNweets((prev) => [nweetObject, ...prev]);
-      //가끔 set이 붙는 함수를 쓸때 값대신 함수를 전달할 수 있다.
-      //그리고 만약 함수를 전달하면 리액트는 이전 값에 접근할 수 있게 해준다
-      //리턴하는것은 implicit return이다, [최근 document , 이전 document들]이 들은 배열을 리턴한다
-    });
-  };
+
   useEffect(() => {
-    getNweets(); //async await 써야하므로 useEffect안에 함수 안적고 따로 위에 뺌
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
   }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("nweets").add({
-      nweet,
+      text: nweet,
       createAt: Date.now(),
+      creatorId: userObj.uid,
     });
     setNweet("");
   };
@@ -36,7 +31,7 @@ const Home = () => {
     //'event로부터'라는 의미, 즉 event안에 있는 target안에 있는 value를 달라고 하는것
     setNweet(value);
   };
-  console.log(nweets);
+  //console.log(nweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -52,7 +47,7 @@ const Home = () => {
       <div>
         {nweets.map((nweet) => (
           <div key={nweet.id}>
-            <h4>{nweet.nweet}</h4>
+            <h4>{nweet.text}</h4>
           </div>
         ))}
       </div>
